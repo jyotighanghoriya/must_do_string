07eb4ca944208e09ac84f0f1e57a4d60e30447af
Jyoti Ghanghoriya <ghanghoriya.jyoti@gmail.com>
2019-07-21 19:55:14 +0530

    Create longest_distinct_character_in_a_string.cpp

---
+++ longest_distinct_character_in_a_string.cpp
@@ -0,0 +1,32 @@
+using namespace std;
+int main()
+ {
+	int t;
+	cin>>t;
+	while(t--){
+	    string s;
+	    cin>>s;
+	    //last index that has seen this character is stored in vis
+	    int vis[26];
+	    for(int i=0; i<26; ++i)
+	        vis[i] = -1;
+	    vis[s[0]-'a'] = 0;
+	    int cur = 1,mx = 1;
+	    for(int i=1; i<s.length(); ++i){
+	        int prev = vis[s[i]-'a'];
+	        if(vis[s[i]-'a']==-1 || i-prev>cur){
+	            cur++;
+	            mx = max(cur,mx);
+	        }
+	        else{
+	            mx = max(cur,mx);
+	            cur = i - prev;
+	        }
+	        vis[s[i]-'a'] = i;
+	    }
+	   //  mx = max(cur,mx);
+	    cout<<mx<<endl;    
+	    
+	}
+	return 0;
+}
