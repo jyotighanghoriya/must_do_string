ef87157bc8f43ed1d92b574c9007190b2e3a873f
Jyoti Ghanghoriya <ghanghoriya.jyoti@gmail.com>
2019-07-23 10:20:27 +0530

    Update reverse_word_in_a_given_string.cpp

--- reverse_word_in_a_given_string.cpp
+++ reverse_word_in_a_given_string.cpp
@@ -1,19 +1,39 @@
 using namespace std;
+
+void revWord(string &s, int b, int e) 
+{ 
+    char temp; 
+    while (b<e) { 
+        temp = s[b]; 
+        s[b++] = s[e]; 
+        s[e--] = temp; 
+    } 
+} 
+
+void reverse(string s){
+    int n = s.length();
+    int b = 0;
+    for(int i=0; i<n; i++){
+        if(s[i]=='\0')
+            revWord(s,b,i-1);
+        else if(s[i]=='.'){
+            revWord(s,b,i-1);
+            b = i+1;
+        }
+    }
+    revWord(s,b,n-1);
+    revWord(s,0,n-1);
+    cout<<s<<endl;
+}
+
 int main()
  {
 	int t;
 	cin>>t;
-	while(t--)
-	{
-		int n,res=0;
-		cin >> n;
-		for(int i=1; i<n; i++){
-		    int x;
-			cin>>x;
-			res = res^x;
-			res = res^i;
-		}
-		cout<<(res^n)<<endl;
+	while(t--){
+	    string s;
+	    cin>>s;
+	    reverse(s);
 	}
 	return 0;
 }
