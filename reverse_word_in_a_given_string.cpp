b00b2cd9571b90d23084335cfcc721bce7fc4d70
Jyoti Ghanghoriya <ghanghoriya.jyoti@gmail.com>
2021-06-08 12:13:54 +0530

    Update reverse_word_in_a_given_string.cpp

--- reverse_word_in_a_given_string.cpp
+++ reverse_word_in_a_given_string.cpp
@@ -1,3 +1,23 @@
+Given a String S, reverse the string without reversing its individual words. Words are separated by dots.
+
+Example 1:
+
+Input:
+S = i.like.this.program.very.much
+Output: much.very.program.this.like.i
+Explanation: After reversing the whole
+string(not individual words), the input
+string becomes
+much.very.program.this.like.i
+Example 2:
+
+Input:
+S = pqr.mno
+Output: mno.pqr
+Explanation: After reversing the whole
+string , the input string becomes
+mno.pqr
+
 using namespace std;
 
 void revWord(string &s, int b, int e) 
@@ -37,3 +57,15 @@ int main()
 	}
 	return 0;
 }
+
+
+Python : 
+class Solution:
+    
+    #Function to reverse words in a given string.
+    def reverseWords(self,S):
+        l=S.split('.')
+        l.reverse()
+        s=".".join(l)
+        return s
+
